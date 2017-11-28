5353c504439cb8600e0a7ea6481d7904a9f4ea4a
fisk <myself@fiskie.me>
2017-11-28 14:40:39 +0000

    day 5

---
+++ day5/day5_part1.py
@@ -0,0 +1,37 @@
+bad_strings = ["ab", "cd", "pq", "xy"]
+vowels = 'aeiou'
+
+
+def is_nice(string):
+    vowel_count = 0
+    has_double = False
+
+    for i in range(0, len(string)):
+        if string[i] in vowels:
+            vowel_count += 1
+
+        if i == 0:  # the next checks require a previous character to exist
+            continue
+
+        if string[i-1] == string[i]:
+            has_double = True
+
+        if (string[i-1] + string[i]) in bad_strings:
+            return False
+
+    return vowel_count >= 3 and has_double
+
+# Sanity test our function with a few asserts based on the example strings given
+assert is_nice('ugknbfddgicrmopn')  # nice string
+assert not is_nice("jchzalrnumimnmhp")  # no double letter
+assert not is_nice("haegwjzuvuyypxyu")  # contains bad string
+assert not is_nice("dvszwmarrgswjxmb")  # contains one vowel
+
+nice_count = 0
+
+with open('day5_input.txt', 'r') as f:
+    for line in f:
+        if is_nice(line):
+            nice_count += 1
+
+print("%d strings are nice!" % nice_count)
---
+++ day5/day5_part2.py
@@ -0,0 +1,31 @@
+def is_nice(string):
+    vowel_count = 0
+    has_repeated_char = False
+    has_repeated_pair = False
+
+    for i in range(0, len(string)):
+        if i >= 2 and string[i - 2] == string[i]:
+            has_repeated_char = True
+
+        if i >= 1:
+            pos = string.find(string[i - 1] + string[i])
+
+            if not pos == -1 and pos < i - 2:
+                has_repeated_pair = True
+
+    return has_repeated_char and has_repeated_pair
+
+# Like with part 1, some more assertion tests
+assert is_nice('qjhvhtzxzqqjkmpb')  # nice string
+assert is_nice("xxyxx")  # nice string, with overlapping rules
+assert not is_nice("uurcxstgmygtbstg")  # no repeated letter
+assert not is_nice("ieodomkazucvgmuy")  # no pair appearing twice
+
+nice_count = 0
+
+with open('day5_input.txt', 'r') as f:
+    for line in f:
+        if is_nice(line):
+            nice_count += 1
+
+print("%d lines are nice!" % nice_count)
